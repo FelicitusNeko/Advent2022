6a0eef8d5d354e71c6ec764160cb0eed93e58cda
Felix <felicitusneko@duck.com>
2022-12-03 11:50:10 -0400

    day template with standard boilerplate

---
+++ src/y2022/DayTemplate.hx
@@ -0,0 +1,13 @@
+package y2022;
+
+class Day {
+  public static function problem1(data:String) {
+    var list = data.split("\n");
+		return Std.string("");
+  }
+
+  public static function problem2(data:String) {
+    var list = data.split("\n");
+		return Std.string("");
+  }
+}
