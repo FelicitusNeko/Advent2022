119a033cd8ab5a5fcefd2f1798240a2bea59b1eb
Felix <felicitusneko@duck.com>
2022-12-04 14:51:37 -0400

    use trim() on data in template

--- src/y2022/DayTemplate.hx
+++ src/y2022/DayTemplate.hx
@@ -1,13 +1,15 @@
 package y2022;
 
+using StringTools;
+
 class Day {
   public static function problem1(data:String) {
-    var list = data.split("\n");
+    var list = data.trim().split("\n");
 		return Std.string("");
   }
 
   public static function problem2(data:String) {
-    var list = data.split("\n");
+    var list = data.trim().split("\n");
 		return Std.string("");
   }
 }
