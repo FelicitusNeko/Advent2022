9034fe2521edd0954ef149ed98c01b01e72ba07c
Felix <felicitusneko@duck.com>
2022-12-12 20:59:41 -0400

    make Point abstract

--- src/y2022/Day9.hx
+++ src/y2022/Day9.hx
@@ -12,9 +12,20 @@ enum MoveInstruction {
 	Left(step:Int);
 }
 
-typedef Point = {
+typedef IPoint = {
 	var x:Int;
 	var y:Int;
+}
+
+@:forward
+abstract Point(IPoint) from IPoint to IPoint {
+	@:to
+	public function toString()
+		return '${this.x}:${this.y}';
+
+	@:op(a == b)
+	public function isEqual(rhs:Point)
+		return this.x == rhs.x && this.y == rhs.y;
 }
 
 class Walker {
