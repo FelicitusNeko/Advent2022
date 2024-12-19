f0384ee812fa25a230be7ef848fa509c3cd7836a
Felix R <felicitusneko@duck.com>
2024-12-19 02:58:29 -0400

    update the template since I've been using a dedicated parse function every day lately

--- src/y2024/DayTemplate.hx
+++ src/y2024/DayTemplate.hx
@@ -5,23 +5,26 @@ using StringTools;
 private var testData = [''];
 
 class Day extends DayEngine {
-  public static function make(data:String) {
+	public static function make(data:String) {
 		var tests = testData.map(i -> {
 			return {
 				data: i,
 				expected: []
 			}
 		});
-		new Day(data, 1, tests);
+		new Day(data, , tests);
 	}
 
-  function problem1(data:String) {
-    var list = data.rtrim().split("\n");
-		return null;
-  }
+	function parse(data:String)
+		return data.rtrim().split("\n");
 
-  function problem2(data:String) {
-    var list = data.rtrim().split("\n");
+	function problem1(data:String) {
+		var list = parse(data);
 		return null;
-  }
+	}
+
+	function problem2(data:String) {
+		var list = parse(data);
+		return null;
+	}
 }
