7811df041903cbcd7d308a06fc75fc3f69c5e72f
Felix R <felicitusneko@duck.com>
2024-12-21 16:06:36 -0400

    I don't know what I'm doing

--- src/y2024/Day21.hx
+++ src/y2024/Day21.hx
@@ -40,12 +40,21 @@ private enum IInstruction {
 	Dir(d:Direction);
 	Activate;
 	Gap;
+	Moves(q:Int, pt:Point);
 }
 
 @:forward
 private abstract Instruction(IInstruction) from IInstruction to IInstruction {
+	public var qty(get, never):Int;
+
 	inline function new(i:IInstruction)
 		this = i;
+
+	function get_qty()
+		return switch (this) {
+			case Moves(q, _): q;
+			default: 1;
+		}
 
 	@:op(a == b)
 	function cmpInt(i:Int)
@@ -84,6 +93,7 @@ private abstract Instruction(IInstruction) from IInstruction to IInstruction {
 			case Dir(d): d.c;
 			case Activate: "A";
 			case Gap: " ";
+			case Moves(q, pt): '{$q=>${pt.toString()}}';
 		}
 
 	@:from
@@ -130,19 +140,21 @@ class Day21 extends DayEngine {
 		var list = parse(data);
 		var retval = 0;
 
-		for (z => code in list) {
-			var bpt:Array<Point> = [[2, 3], [2, 0], [2, 0]];
+		var bots = 3;
+		var cache:Array<Map<String, Int>> = [for (_ in 0...bots) []];
 
+		for (z => code in list) {
 			function nav(bot:Int, inst:Array<Instruction>) {
-				if (bot >= 3)
+				if (bot >= bots)
 					return inst;
 				var gaprow = bot == 0 ? 3 : 0;
 				var pt:Point = [2, gaprow];
 				// 789 / 456 / 123 / x0A
 				// x^A / <v>
 
+				var warp:Array<Int> = [];
 				var dstlist = [
-					for (i in inst)
+					for (z => i in inst)
 						Point.fromArray(switch (i) {
 							case Num(n): [n == 0 ? 1 : ((n - 1) % 3), 3 - Math.ceil(n / 3)];
 							case Dir(d): switch (d) {
@@ -152,29 +164,39 @@ class Day21 extends DayEngine {
 									case Right: [2, 1];
 								}
 							case Activate: [2, gaprow];
+							case Moves(_, pt):
+								warp.push(z);
+								[pt.x, pt.y];
 							case Gap: throw new Exception("Instruction cannot be gap");
 						})
 				];
-				if (bot == 0) trace(code, pt, dstlist);
+				if (bot == 0)
+					trace(code, pt, dstlist);
 
 				var retval:Array<Instruction> = [];
-				for (dst in dstlist) {
-					function move(f:Int, t:Int, d:Array<Direction>)
-						for (_ in 0...Math.round(Math.abs(f - t))) retval.push(Dir(d[f > t ? 0 : 1]));
-					var yfirst = pt.y == gaprow;
-					if (yfirst) move(pt.y, dst.y, [Up, Down]);
-					move(pt.x, dst.x, [Left, Right]);
-					if (!yfirst) move(pt.y, dst.y, [Up, Down]);
-					retval.push(Activate);
-					pt = dst;
+				for (z => dst in dstlist) {
+					if (warp.contains(z)) {} else {
+						inline function move(f:Int, t:Int, d:Array<Direction>)
+							for (_ in 0...Math.round(Math.abs(f - t)))
+								retval.push(Dir(d[f > t ? 0 : 1]));
+						var yfirst = pt.y == gaprow;
+						if (yfirst)
+							move(pt.y, dst.y, [Up, Down]);
+						move(pt.x, dst.x, [Left, Right]);
+						if (!yfirst)
+							move(pt.y, dst.y, [Up, Down]);
+						retval.push(Activate);
+						pt = dst;
+					}
 				}
 
-				//if (z == 2) trace(retval.map(i -> i.toString()).join(""));
+				if (bot < 2)
+					trace(retval.map(i -> i.toString()).join(""));
 				return nav(bot + 1, retval);
 			}
 
 			var myinput = nav(0, code.split("").map(Instruction.fromString));
-			trace(myinput.length, Std.parseInt(code), myinput.length * Std.parseInt(code));
+			// trace(myinput.length, Std.parseInt(code), myinput.length * Std.parseInt(code));
 			retval += myinput.length * Std.parseInt(code);
 		}
 
