1615859d6513a0ad232cfb3775a624b173220e40
Felix R <felicitusneko@duck.com>
2025-12-03 03:07:58 -0400

    removed commented code, added docs to 2025.2.2

--- src/y2025/Day2.hx
+++ src/y2025/Day2.hx
@@ -51,21 +51,16 @@ class Day2 extends DayEngine {
 				ey = Std.parseInt(item.end.substring(ehalf));
 			var hasFoundInvalid = false;
 
-			// trace('Start comparing ${item.start} ($sx $sy:$slen $shalf $slimit) to ${item.end} ($ex $ey:$elen $ehalf)');
-
 			while (slen < elen || sx < ex || (sx == ex && sy <= ey)) {
 				if (slen % 2 == 0 && sx == sy) {
 					hasFoundInvalid = true;
-					// trace('$sx${Std.string(sy).lpad('0', shalf)} is invalid');
 					total += Int64Helper.parseString('$sx${Std.string(sy).lpad('0', shalf)}');
 				}
 				sy++;
 				if (sy >= slimit) {
-					// trace('rounding $sy ($slimit)');
 					sy -= slimit;
 					var oldlen = Std.string(sx++).length;
 					if (sx >= slimit) {
-						// trace('shifting middle point $sx $sy');
 						sy += slimit * (sx % 10);
 						sx = Math.floor(sx / 10);
 						slimit *= 10;
@@ -78,7 +73,6 @@ class Day2 extends DayEngine {
 				if (hasFoundInvalid)
 					sx++;
 			}
-			// trace('$debugFoundInvalid invalid found');
 		}
 		return total;
 	}
@@ -90,18 +84,22 @@ class Day2 extends DayEngine {
 
 		for (item in list) {
 			var slen = item.start.length, elen = item.end.length;
-			//trace('Testing ${item.start}-${item.end}');
+			// we don't need to evaluate for segments more than half of the number's length
 			for (seglen in 1...Math.ceil(elen / 2) + 1) { 
-				// trace('Testing ${item.start} with slices of $seglen');
-				if (slen < seglen)
+				if (slen < seglen) // probably a redundant check but cheap
 					break;
+				// if neither the length of the start or end is a length a multiple of seglen,
+				// we're probably not finding any results
+				// this could break if for example slen is 11 and elen is 13 on a seglen of 4
 				if (slen % seglen != 0 && elen % seglen != 0)
 					continue;
 
 				function split(num:String, chars:Int) {
+					// to simplify code a bit, extend the length of the number to a multiple of seglen
 					var pnum = num.lpad("0", Math.ceil(num.length / chars) * chars);
 					var pos = 0;
 					var retval:Array<Int> = [];
+					// parseInt every chunk of seglen chars and stick it into an array
 					while (pos < pnum.length) {
 						retval.push(Std.parseInt(pnum.substring(pos, pos + chars)));
 						pos += chars;
@@ -109,6 +107,7 @@ class Day2 extends DayEngine {
 					return retval;
 				}
 
+				// rough little comparator for Array<Int>s representing larger numbers
 				function compare(lhs:Array<Int>, rhs:Array<Int>) {
 					if (lhs.length < rhs.length)
 						return 1;
@@ -126,32 +125,37 @@ class Day2 extends DayEngine {
 				var pos:Array<Int> = split(item.start, seglen),
 					end:Array<Int> = split(item.end, seglen);
 				var poslen = pos.length;
-				var limit = Math.round(Math.pow(10, seglen));
+				var limit = Math.round(Math.pow(10, seglen)); // e.g. a seglen of 4 wraps at 10000 (10⁴)
 				var match = 1;
-				// trace(pos, end, compare(pos, end));
 
-				while (compare(pos, end) >= 0) {
-					// trace(pos, pos[poslen - match], pos[poslen - match - 1], pos[poslen - match] == pos[poslen - match - 1]);
+				while (compare(pos, end) >= 0) { // while pos < end, effectively
+					// evaluate from end to start if numbers in the array are equal
+					// because each number that matches will tick at the same time
 					while (match < poslen && pos[poslen - match] == pos[poslen - match - 1])
 						match++;
-					if (match > 1 && match == poslen && Std.string(pos[0]).length == seglen && !ids.exists(pos.join(""))) {
-						//trace('${pos.join("")} is invalid');
+					// if every number in the array matches such that zero-padding wouldn't
+					// result in a leading zero, provided of course there's more than one number,
+					// add it to the list of invalid ids if it's not there already
+					if (match > 1 && match == poslen && Std.string(pos[0]).length == seglen)
 						ids.add(pos.join(""));
-					}
+					// tick every matching number, wrapping at the value of limit
 					for (x in 1...match + 1)
 						pos[poslen - x] = (pos[poslen - x] + 1) % limit;
+
+					// if the value of the last number is 0, we need to carry the 1
 					if (pos[poslen - 1] == 0) {
+						// if all the numbers are already matching, add a chunk to the list
 						if (match == poslen) {
 							pos.unshift(1);
 							poslen++;
-						} else
+						} else // otherwise, carry the 1 to the first non-matching chunk from the end
 							pos[poslen - match - 1]++;
 					}
 				}
 			}
 		}
 		for (id in ids)
-			total += Int64Helper.parseString(id);
-		return total;
+			total += Int64Helper.parseString(id); // add 'em up
+		return total; // and there's your number
 	}
 }
